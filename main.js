0f1f5c83dbdd4f6b2a8c6a7fc09bb2eb3d9a78db
vub <vub@gmail.com>
2013-08-18 11:07:24 -0400

    Added multisig support

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ var cp = require('child_process'),
 
 require('long-stack-traces');
 
+var m = {};
+
 var strip = function(s) { return s.replace(/^\s+|\s+$/g, '') }
 var identity = function(x) { return x; }
 
-var errHandle = eh = function(fail, success) {
+var eh = m.errHandle = function(fail, success) {
     return function(err, res) {
         if (err) {
             //console.log('e',err,'f',fail,'s',success);
@@ -22,18 +24,17 @@ var errHandle = eh = function(fail, success) {
     };
 };
 
-var cbuntil = function(f,cb) {
+m.cbuntil = function(f,cb) {
     f(eh(cb,function(res) {
-        if (!res) { cbuntil(f,cb) }
+        if (!res) { m.cbuntil(f,cb) }
         else cb(null,res);
     }));
 }
 
-var cbmap = function(array,f,cb) {
-    var cbs = 0;
+m.cbmap = function(array,f,cb) {
+    var cbs = array.length;
     var out = Array(array.length);
     for (var i = 0; i < array.length; i++) {
-        cbs++;
         f(array[i],function(ii) { return eh(cb,function(v) {
             out[ii] = v;
             cbs--;
@@ -42,7 +43,7 @@ var cbmap = function(array,f,cb) {
     };
 }
 
-var cbmap_seq = function(array,f,cb) {
+m.cbmap_seq = function(array,f,cb) {
     var out = Array(array.length);
     var inner = function(i) {
         f(array[i],function(ii) { return eh(cb,function(v) {
@@ -66,8 +67,6 @@ var cmdcall = function(arg,args,inp,cb) {
     p.stdout.on('close',function() { cb(null,strip(data)); });
     p.stdout.on('error',cb);
 }
-
-var m = {};
 
 m.newkey = _.partial(cmdcall,'newkey',null,null);
 m.pubkey = _.partial(cmdcall,'pubkey',null);
@@ -126,11 +125,14 @@ m.history = function(addr,cb) {
 
 m.scripthash = _.partial(cmdcall,'scripthash',null);
 m.rawscript = function(inp,cb) { cmdcall('rawscript',inp,null,cb); }
+m.showscript = function(inp,cb) { 
+    cmdcall('showscript',null,inp,eh(cb,function(s) { cb(null,strip(s).split(' ')); }));
+}
 
 var txop = function(arg, args, output_tx, tx, inp, cb) {
     async.waterfall([function(cb2) {
         var filename = '/tmp/sxnode-' + (""+Math.random()).substring(2,11);
-        fs.writeFile(filename,tx || "",errHandle(cb,function() { cb2(null,filename); }));
+        fs.writeFile(filename,tx || "",eh(cb,function() { cb2(null,filename); }));
     }, function(filename,cb2) {
         args = [filename].concat(args || []);
         //console.log('fn',filename,'args',args,'a',arguments);
@@ -160,44 +162,90 @@ m.mktx = function(inputs, outputs, cb) {
     txop('mktx',args,true,null,null,cb);
 }
 
-m.showtx = function(tx,cb) { txop("showtx",null,false,tx,null,cb); }
+m.showtx = function(tx,cb) { 
+    txop("showtx",null,false,tx,null,eh(cb,function(shown) {
+        try {
+            var fields = shown.split('\n')
+                .map(function(str) { return str.split(' ').filter(identity); })
+    
+            var inputs = []; outputs = [];
+            for (var i = 0; i < fields.length;) {
+                if (fields[i][0] == "Input:") {
+                    inputs.push({
+                        prev: fields[i+1][2],
+                        script: fields[i+2].slice(1,fields[i+2].length-1),
+                        sequence: parseInt(fields[i+2][fields[i+2].length-1]),
+                        address: (27 < (fields[i+3][1]||"").length < 34) ? fields[i+3][1] : null
+                    }); i += 3;
+                }
+                else if (fields[i][0] == "Output:") {
+                    outputs.push({
+                        value: parseInt(fields[i+1][1]),
+                        script: fields[i+2].slice(1),
+                        address: fields[i+3][1]
+                    }); i += 4;
+                }
+                else i++;
+            }
+            cb(null, { inputs: inputs, outputs: outputs });
+        } catch(e) { cb(e); }
+    }));
+}
+
+m.addr_to_pubkey = m.address_to_pubkey = function(addr,cb) {
+    m.history(addr,eh(cb,function(h) {
+        h = h.filter(function(o) { return o.spend });
+        if (!h.length) {
+            return cb("No spends from this address"); 
+        }
+        m.fetch_transaction(h[0].spend.substring(0,64),eh(cb,function(tx) {
+            m.showtx(tx,eh(cb,function(shown) {
+                var pub = shown.script[4];
+                if (len(pub) == 66 || len(pub) == 130) { return cb(null,pub); }
+                return cb("Failed to parse script: "+shown.script);
+            }));
+        }));
+    }));
+}
 
 m.sign_input = function(tx,index,script,key,cb) { txop("sign-input",[index,script],false,tx,key,cb); }
 
-m.set_input = function(tx,index,inp,cb) { txop("set-input",[index],true,tx,inp,cb); }
+m.set_input = function(tx,index,inp,cb) { txop("set-input",[index],false,tx,inp,cb); }
 
 m.broadcast = m.broadcast_tx = function(tx,cb) {
     async.waterfall([function(cb2) {
         var filename = '/tmp/sxnode-' + (""+Math.random()).substring(2,11);
-        fs.writeFile(filename,tx || "",errHandle(cb,function() { cb2(null,filename); }));
+        fs.writeFile(filename,tx || "",eh(cb,function() { cb2(null,filename); }));
     }, function(filename,cb2) {
         var p = spawn("sx",['broadcast-tx',filename]);
         p.stdin.end();
         var data = "";
-        p.stdout.on('data',function(d) { data += d; console.log(""+d); });
+        var count = 0;
+        p.stdout.on('data',function(d) { 
+            data += d; count++; console.log(""+d);
+            if (count >= 25) { return cb2(null,data); }
+        });
         p.stdout.on('close',function() { cb2(null,data); });
         p.stdout.on('error',cb);
     }],cb);
 }
 
-m.get_enough_utxo = function(address,amount,cb) {
-    m.history(address,eh(cb,function(h) {
-        //console.log('ww',address,amount,cb,h);
-        var utxo = h.filter(function(x) { return !x.spend });
-        var valuecompare = function(a,b) { return a.value > b.value; }
-        var high = utxo.filter(function(o) { return o.value >= amount; }).sort(valuecompare);
-        if (high.length > 0) { return cb(null,high[0]); }
-        utxo.sort(valuecompare);
-        var totalval = 0;
-        for (var i = 0; i < utxo.length; i++) {
-            totalval += utxo[i].value;
-            if (totalval >= amount) { return cb(null,utxo.slice(0,i+1)); }
-        }
-        return cb({ err: "Not enough money", value: totalval, needed: amount});
-    }));
+m.get_enough_utxo = function(h,amount,cb) {
+    //console.log('ww',address,amount,cb,h);
+    var utxo = h.filter(function(x) { return !x.spend });
+    var valuecompare = function(a,b) { return a.value > b.value; }
+    var high = utxo.filter(function(o) { return o.value >= amount; }).sort(valuecompare);
+    if (high.length > 0) { return cb(null,[high[0]]); }
+    utxo.sort(valuecompare);
+    var totalval = 0;
+    for (var i = 0; i < utxo.length; i++) {
+        totalval += utxo[i].value;
+        if (totalval >= amount) { return cb(null,utxo.slice(0,i+1)); }
+    }
+    return cb({ err: "Not enough money", value: totalval, needed: amount});
 }
 
-m.make_sending_transaction = function(prevs, to, value, change, cb) {
+m.make_sending_transaction_from_utxo = function(prevs, to, value, change, cb) {
     var sum = prevs.map(function(x) { return x.value; })
         .reduce(function(a,b) { return a+b; },0);
     var outputs = [{
@@ -213,6 +261,15 @@ m.make_sending_transaction = function(prevs, to, value, change, cb) {
         });
     }
     m.mktx(prevs,outputs,cb);
+}
+
+m.make_sending_transaction = function(from, to, value, change, cb) {
+    if (!change) change = from;
+    m.history(from,eh(cb,function(h) {
+        m.get_enough_utxo(h,value,eh(cb,function(utxo) {
+            m.make_sending_transaction_from_utxo(utxo, to, value, change, cb);
+        }));
+    }));
 }
 
 m.txhash = function(tx,cb) {
@@ -235,11 +292,39 @@ m.gen_addr_data = function(pk,cb) {
         function(cb2) {
             if (addrdata.addr[0] == '1') {
                 var script = ['dup','hash160','[',addrdata.hash160,']','equalverify','checksig'];
+                addrdata.script = script;
                 m.rawscript(script, adpusher(addrdata,'raw',cb2));
             }
             else cb2();
         },
     ],eh(cb,function() { cb(null,addrdata); }));
+}
+
+m.gen_multisig_addr_data = function(pubs,k,cb) {
+    m.cbmap(pubs,function(addrdata,cb2) {
+        if (typeof addrdata == "object") { 
+            return cb2(null,addrdata.pub);
+        }
+        addrdata = strip(addrdata);
+        if (addrdata.length == 66 || addrdata.length == 130) {
+            cb2(null,addrdata);
+        }
+        else if (addrdata.length > 35 && addrdata.length < 66) {
+            m.pubkey(addrdata,cb2);
+        }
+        else {
+            console.log("Address given. Attempting to find pubkey in blockchain");
+            m.addr_to_pubkey(addrdata,cb2);
+        }
+    },eh(cb,function(results) {
+        var script = Array.prototype.concat.apply([k],results.map(function(pub) { return ['[',pub,']']; }))
+            .concat([results.length,'checkmultisig']);
+        m.rawscript(script,eh(cb,function(raw) {
+            m.scripthash(raw,eh(cb,function(addr) {
+                cb(null,{ raw: raw, script: script, addr: addr, n: results.length, k: k });
+            }));
+        }));
+    }));
 }
 
 m.sign_tx_input = function(tx,index,addrdata,cb) {
@@ -249,7 +334,6 @@ m.sign_tx_input = function(tx,index,addrdata,cb) {
         }));
     }
     if (!addrdata.raw) { return cb("Malformed addrdata object or multisig address"); }
-    console.log('si',tx,index);
     m.sign_input(tx,index,addrdata.raw,addrdata.priv,eh(cb,function(sig) {
         var script = ['[',sig,']','[',addrdata.pub,']'];
         m.rawscript(script,eh(cb,function(raw2) {
@@ -260,23 +344,76 @@ m.sign_tx_input = function(tx,index,addrdata,cb) {
     }));
 }
 
-m.multisig_sign_tx_input = function(tx,index,addrdata,cb) {
+//Data can also be the script
+m.apply_multisignatures_at_index = function(tx,index,sigobj,data,cb) {
+    if (typeof data == "string") {
+        return m.showscript(data,eh(cb,function(sc) {
+            var n = parseInt(sc[sc.length-2]),
+                k = parseInt(sc[0]);
+            m.apply_multisignatures_at_index(tx,index,sigobj,{ script: data, n: n, k: k },cb);
+        }));
+    }
+    var keys = Object.keys(sigobj).sort(function(a,b) { return a > b; });
+    var sigs = [];
+    for (var i = 0; i < keys.length; i++) {
+        sigs.push(sigobj[keys[i]]);
+    }
+    var zeroes = [].concat(_.range(data.k,data.n).map(function() { return 'zero' }));
+    var script = [].concat.apply(zeroes,sigs.map(function(sig) { return ['[',sig,']']; }))
+        .concat(['[',data.script,']']);
+    m.rawscript(script,eh(cb,function(r) {
+        m.set_input(tx,index,r,eh(cb,function(ss) { cb(null,ss)}));
+    }));
+    
+}
+
+//Data can also be the script
+m.apply_multisignatures = function(tx,sigs,data,cb) {
+    m.showtx(tx,eh(cb,function(shown) {
+        var o = { tx: tx }
+        m.cbmap_seq(_.range(shown.inputs.length),function(i,cb2) {
+            var isigs = {};
+            for (var ind in sigs) { isigs[ind] = sigs[ind][i]; }
+            m.apply_multisignatures_at_index(o.tx,i,isigs,data,eh(cb,function(tx) {
+                o.tx = tx;   
+                cb2();
+            }));
+        },eh(cb,function() { cb(null, o.tx); }));
+    }));
+}
+
+m.multisig_sign_tx_input = m.sign_input;
+
+m.multisig_sign_tx_inputs = function(tx,script,pk,cb) {
+    m.showtx(tx,eh(cb,function(shown) {
+        m.cbmap(_.range(shown.inputs.length),function(i,cb3) {
+            m.multisig_sign_tx_input(tx,i,script,pk,cb3);
+        },eh(cb,function(sigs) {
+            m.showscript(script,eh(cb,function(shown2) {
+                m.pubkey(pk,eh(cb,function(pub) {
+                    var i = shown2.filter(function(x) { 
+                        return x.length == 66 || x.length == 130; 
+                    }).indexOf(pub);
+                    var o = {};
+                    o[i] = sigs;
+                    return cb(null,o);
+                }));
+            }));
+        }));
+    }));
 }
 
 m.send = function(pk, to, value, change, cb) {
     async.waterfall( [
         function(cb2) { m.gen_addr_data(pk,cb2); },
         function(addrdata,cb2) { 
-            console.log('u',addrdata);
-            m.get_enough_utxo(addrdata.addr,value,eh(cb,function(utxo) { 
-                m.make_sending_transaction(utxo, to, value, change, eh(cb,function(tx) {
-                    cb2(null,addrdata,utxo,tx);
-                }));
+            m.make_sending_transaction(addrdata.addr, to, value, change, eh(cb,function(tx) {
+                cb2(null,addrdata,utxo,tx);
             }));
         },
         function(addrdata,utxo,tx,cb2) {
             addrdata.tx = tx;
-            cbmap_seq(_.range(utxo.length),function(i,cb3) {
+            m.cbmap_seq(_.range(utxo.length),function(i,cb3) {
                 m.sign_tx_input(addrdata.tx,i,addrdata,eh(cb,function(tx) {
                     addrdata.tx = tx;
                     cb3();
@@ -290,6 +427,4 @@ m.send = function(pk, to, value, change, cb) {
     }],cb);
 }
 
-m.errHandle = errHandle;
-m.cbuntil = cbuntil;
 module.exports = m;
--- test.js
+++ test.js
@@ -7,7 +7,66 @@ _ = require('underscore');
 
 var dl = { listener: function(){} }
 
+var dls = [ function(){} ];
+var redirector = function(i) { return function(cb) {  dls[i](cb) }; }
+
+function pod(cb) {
+    dls[dls.length - 1] = function(){};
+    dls.push(cb);
+    process.stdin.on('data',redirector(dls.length-1));
+}
+
 async.waterfall([function(cb) {
+    var seed = 'a45ab3566367728909a778482e328b0d';
+    sx.cbuntil(function(cb2) {
+        console.log("REPL");
+        pod(function(text) {
+            var fields = (text+"").split(' ');
+            if (fields[0] == "addr") {
+                sx.genpriv(seed,parseInt(fields[1]),0,eh(cb,function(pk) {   
+                    sx.gen_addr_data(pk,eh(cb,function(data) {
+                        console.log(data);
+                        return cb2(null,false);
+                    }));
+                }));
+            }
+            else if (fields[0] == "msigaddr") {
+                sx.gen_multisig_addr_data(fields.slice(1,4),2,eh(cb,function(data) {
+                    console.log(data);
+                    return cb2(null,false);
+                }));
+            }
+            else if (fields[0] == "mkmultitx") {
+                sx.make_sending_transaction(fields[1],fields[2],parseInt(fields[3]),null,eh(cb,function(tx) {
+                    console.log("Transaction: ",tx);
+                    return cb2(null,false);
+                }));
+            }
+            else if (fields[0] == "sign") {
+                sx.multisig_sign_tx_inputs(fields[1],fields[2],fields[3],eh(cb,function(sigs) {
+                    console.log(JSON.stringify(sigs));
+                    return cb2(null,false);
+                }));
+            }
+            else if (fields[0] == "apply") {
+                var sigs = JSON.parse(fields.slice(3).join(' ').replace("'",'"'));
+                sx.apply_multisignatures(fields[1],sigs,fields[2],eh(cb,function(tx) {
+                    console.log(tx);
+                    return cb2(null,false);
+                }));
+            }
+            else if (fields[0] == "broadcast") {
+                sx.broadcast(fields[1],eh(cb,function(data) {
+                    return cb2(null,false);
+                }));
+            }
+            else if (fields[0] == "exit") { return cb2(null,true); }
+            else { console.log("Error, bad command"); return cb2(null,false); }
+        });
+    });
+}]);
+
+/*async.waterfall([function(cb) {
     seed = 'a45ab3566367728909a778482e328b0d'
     console.log("Choose Electrum wallet address index");
     process.stdin.on('data',function(text) { dl.listener(text); });
@@ -49,4 +108,4 @@ async.waterfall([function(cb) {
     sx.send(addrdata.pk,'1VubN5ipWkCpcQ3pn7c74FNhfnzo4vg3D',100000,addrdata.addr,eh(cb,function(tx) {
         console.log("Success: ",tx);
     }));
-}],function(err,res) { console.log(err ? err : res); });
+}],function(err,res) { console.log(err ? err : res); });*/
